6c5c7fba80f032638cd074b56cdfaf6d4c39efe5
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-03-13 21:36:42 +0900

    fix

---
+++ src/FaceRecognitionDotNet.Front/wwwroot/js/canvas.js
@@ -0,0 +1,18 @@
+﻿var canvas;
+var context;
+var img = new Image();
+
+function canvas_init(base64)
+{
+    canvas = document.getElementById("SrcCanvas");
+    context = canvas.getContext("2d");
+
+    img.onload = function ()
+    {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+    }
+
+    img.src = base64;
+}
